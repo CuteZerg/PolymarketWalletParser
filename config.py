a1f08d79a3788abd9cc6d9e2055fefe91249cea0
Alexey Krasnikov <creeper.kr@gmail.com>
2026-09-22 06:17:21 +0300

    Adjust chunk size and concurrency settings for optimized performance

--- config.py
+++ config.py
@@ -15,6 +15,6 @@ TRANSFER_SINGLE_TOPIC0 = '0x' + Web3.keccak(text="TransferSingle(address,address
 TRANSFER_BATCH_TOPIC0 = '0x' + Web3.keccak(text="TransferBatch(address,address,address,uint256[],uint256[])").hex() # For ERC1155 (batch transfers)
 
 START_BLOCK = 80813682 - 5 # Starting block for indexing, subtracting 5 to ensure we capture any missed events due to reorgs or delays.
-CHUNK_SIZE = 2000
-MAX_CONCURRENT_REQUESTS = 3
+CHUNK_SIZE = 1000
+MAX_CONCURRENT_REQUESTS = 1
 RATE_LIMIT_DELAY = 1
